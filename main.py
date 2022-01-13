aa035355eb17462bebc6a35669d0bbf62cae4a13
Arif Efendi <97644171+Mr-Arief@users.noreply.github.com>
2022-01-13 09:07:33 +0700

    Ttt

---
+++ main.py
@@ -0,0 +1,25 @@
+def main():
+  kontol=input("Nama Kamu Siapa? ==> ")
+  print("Welcomee ", kontol)
+hu='''
+=============================
+============WELCOME==========
+-----------------------------
+'''
+def pilihan():
+  u='''
+  Kamu sayang sama aku nggak?
+  [1]. Sayang
+  [2]. Nggak
+  [3]. Tidak dipastikan
+  '''
+  print(u)
+  o=input("[~] Jawabanmu nomor.. ")
+  print(o)
+  if(o=='1'):print("Aku Juga Sayang Kamu")
+  elif(o=='2'):print("Padahal Aku Sayang Loh")
+  elif(o=='3'):print("Jawabannya aku tunggu ya")
+  else:pilihan()
+print(hu)
+main()
+pilihan()
