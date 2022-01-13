31fa451186790b6126aca24c0a2c5a3cbf62d289
Arif-User <97644171+Arif-User@users.noreply.github.com>
2022-01-13 08:01:23 +0700

    membuat main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+  kontol=input("Nama Kamu Siapa? ==> ")
+  print("Welcomee ", kontol)
+hu='''
+=============================
+============WELCOME==========
+-----------------------------
+'''
+print(hu)
+main()
